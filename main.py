2296776885329e63874daf1a313f259a314ab095
Alma Vences <avences11@outlook.com>
2025-11-30 01:04:30 -0600

    made some changes

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def load_transactions(file):
         df = pd.read_csv(file)
         df.columns = [col.strip() for col in df.columns]
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
-        df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d") 
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y") 
 
         
 
 
-        st.write(df)
+        
         return df 
     except Exception as e:
         st.error(f"Error loading file: {str(e)}")
@@ -40,7 +40,21 @@ def main():
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
 
-main()
+        if df is not None: 
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy
+            
+            tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+            with tab1: 
+                st.write(debits_df)
+
+            with tab2: 
+                st.write(credits_df)
+
+
+
+
+main() 
 
 
 
